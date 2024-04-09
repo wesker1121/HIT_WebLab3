dec5d6a53d5e63deed656e6474059c0ec1d03bab
wesker1121 <yu1320341213@outlook.com>
2024-04-09 11:13:18 +0800

    v0.1

---
+++ src/views/HomeView.vue
@@ -0,0 +1,104 @@
+<script setup>
+</script>
+
+<template>
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column fixed prop="sid" label="学号" width="150" />
+      <el-table-column prop="name" label="姓名" width="120" />
+      <el-table-column prop="gender" label="性别" width="120" />
+      <el-table-column prop="age" label="年龄" width="120" />
+      <el-table-column prop="birthday" label="生日" width="120" />
+      <el-table-column fixed="right" label="操作" width="120">
+        <template #default="scope">
+          <el-button link type="primary" size="small" @click="handleModify(scope.$index)">修改</el-button>
+          <el-button link type="primary" size="small" @click="handleDelete(scope.$index)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+</template>
+
+
+
+<script setup>
+import { ElMessage, ElMessageBox } from 'element-plus'
+import { ref } from 'vue'
+
+const handleModify = (index) => {
+  ElMessage({
+    type: 'info',
+    message: `modify ${index}`,
+  })
+}
+
+const handleDelete = (index) => {
+  ElMessageBox.confirm(
+    'proxy will permanently delete the file. Continue?',
+    'Warning',
+    {
+      confirmButtonText: 'OK',
+      cancelButtonText: 'Cancel',
+      type: 'warning',
+    }
+  )
+    .then(() => {
+      tableData.value.splice(index, 1)
+      ElMessage({
+        type: 'success',
+        message: 'Delete completed',
+      })
+    })
+    .catch(() => {
+      ElMessage({
+        type: 'info',
+        message: 'Delete canceled',
+      })
+    })
+}
+
+
+const tableData = ref([
+  {
+    sid: '2016-05-02',
+    name: 'alcice',
+    gender:'f',
+    age: '20',
+    birthday: '1996-05-02',
+  },
+  {
+    sid: '2016-05-02',
+    name: 'bb',
+    gender:'f',
+    age: '20',
+    birthday: '1996-05-02',
+  },
+  {
+    sid: '2016-05-02',
+    name: 'cc',
+    gender:'f',
+    age: '20',
+    birthday: '1996-05-02',
+  },
+  {
+    sid: '2016-05-02',
+    name: 'dd',
+    gender:'f',
+    age: '20',
+    birthday: '1996-05-02',
+  },
+  {
+    sid: '2016-05-02',
+    name: 'ee',
+    gender:'f',
+    age: '20',
+    birthday: '1996-05-02',
+  },
+  {
+    sid: '2017-05-02',
+    name: 'ff',
+    gender:'f',
+    age: '20',
+    birthday: '1996-05-02',
+  },
+])
+</script>
+
---
+++ src/views/LoginView.vue
@@ -0,0 +1,37 @@
+<script setup>
+import axios from 'axios';
+import {ref} from 'vue'
+import {useRouter} from 'vue-router'
+import qs from 'querystring'
+let username = ref('')
+let password = ref('')
+const router = useRouter();
+
+function handleLogin(){
+    let data = {
+    username: username.value,
+    password: password.value
+}
+    axios.post("http://localhost:8080/login",qs.stringfy(data))
+    .then((res) => {
+        sessionStorage.setItem("username", username);
+        router.replace('/')
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+}
+    if(username.value == 'admin' && password.value == '123'){
+        sessionStorage.setItem("username", username);
+        router.replace('/');
+    
+    }
+
+</script>
+
+
+<template>
+    <el-input v-model="username" style="width: 240px" placeholder="用户名" /><br>
+    <el-input v-model="password" type="password" style="width: 240px" placeholder="密码" /><br>
+    <el-button type="primary" @click="handleLogin">登录</el-button>
+</template>
